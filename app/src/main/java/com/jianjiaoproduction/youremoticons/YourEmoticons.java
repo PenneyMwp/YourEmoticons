400736f95c0e02424ccdcc2cc3ef3fa7726bfae2
Zhibin-Zhang <henrychuang123@gmail.com>
2016-04-14 22:54:03 -0400

    Added 6 emoticons in the keyboard

--- app/src/main/java/com/jianjiaoproduction/youremoticons/YourEmoticons.java
+++ app/src/main/java/com/jianjiaoproduction/youremoticons/YourEmoticons.java
@@ -11,6 +11,12 @@ import android.view.inputmethod.InputConnection;
  * Created by Zhibin on 4/14/2016.
  */
 public class YourEmoticons extends InputMethodService implements KeyboardView.OnKeyboardActionListener{
+    final String EMOTICON_1 = "¯\\_(ツ)_/¯";
+    final String EMOTICON_2 = "╮(╯▽╰)╭";
+    final String EMOTICON_3 = "(╯‵□′)╯︵┴─┴";
+    final String EMOTICON_4 = "┬─┬ ノ( ' - 'ノ)";
+    final String EMOTICON_5 = "∠( ᐛ 」∠)＿";
+    final String EMOTICON_6 = "_(:3 」∠ )_";
     @Override
     public View onCreateInputView() {
         kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
@@ -34,28 +40,28 @@ public class YourEmoticons extends InputMethodService implements KeyboardView.On
     @Override
     public void onKey(int i, int[] ints) {
         InputConnection ic = getCurrentInputConnection();
-        String emoString = "";
+        String emoString;
         switch(i){
             case 1:
-                emoString = getResources().getString(R.string.emoticon_1);
+                emoString = EMOTICON_1;
                 break;
             case 2:
-                emoString = getResources().getString(R.string.emoticon_2);
+                emoString = EMOTICON_2;
                 break;
             case 3:
-                emoString = getResources().getString(R.string.emoticon_3);
+                emoString = EMOTICON_3;
                 break;
             case 4:
-                emoString = getResources().getString(R.string.emoticon_4);
+                emoString = EMOTICON_4;
                 break;
             case 5:
-                emoString = getResources().getString(R.string.emoticon_5);
+                emoString = EMOTICON_5;
                 break;
             case 6:
-                emoString = getResources().getString(R.string.emoticon_6);
+                emoString = EMOTICON_6;
                 break;
             default:
-                emoString = getResources().getString(R.string.emoticon_1);
+                emoString = "";
         }
 
         ic.commitText(emoString, 1);
